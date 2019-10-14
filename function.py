983492c6451f0d8d4cbe781c9db7adc11ee72f96
Pivi14 <pivarcsiadam@gmail.com>
2019-10-14 19:43:44 +0200

    Change board function, add main modul and function

--- function.py
+++ function.py
@@ -5,34 +5,38 @@ def init_board():
         matrix.append(row)
         for y in range(3):
             row.append(0)
-    board = matrix
-    for i in range(len(matrix)):
-        for y in range(len(matrix[i])):
-            if matrix[i][y] == 0:
-                board[i][y] = '.'
-            elif matrix[i][y] == 1:
-                board[i][y] = 'X'
-            elif matrix[i][y] == 2:
-                board[i][y] = 'O'
-    return board
+    return matrix
 
 def get_move():
+    board = init_board()
+    a = ['A', 'B', 'C']
+    b = [1, 2, 3]
     row = None
     col = None
-    while row == 'A' or 'B' or 'C':
-        try:
-            row = input("Give me the row's letter: ").upper()
-        except:
-            continue
-    while col == 1 or 2 or 3:
-        try:
-            col = int(input("Give me the column's number: "))
-        except:
-            continue
+    while board[a.index(row)][b.index(col)] != 0:
+        while not row in a:
+            try:
+                row = input("Give me the row's letter: ").upper()
+            except:
+                continue
+        while not col in b:
+            try:
+                col = int(input("Give me the column's number: "))
+            except:
+                continue
     coord = (row, col)
     return coord
 
-def print_board(board):
+def print_board(x):
+    board = x
+    for i in range(len(x)):
+        for y in range(len(x[i])):
+            if x[i][y] == 0:
+                board[i][y] = '.'
+            elif x[i][y] == 1:
+                board[i][y] = 'X'
+            elif x[i][y] == 2:
+                board[i][y] = 'O'
     print('   1   2   3')
     print("A", ' {:1} | {:1} | {:1}'.format(board[0][0], board[0][1], board[0][2]))
     print('  ---+---+---')
