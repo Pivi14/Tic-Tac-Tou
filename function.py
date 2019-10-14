c896e9094df2dd62554a5c5a634d92abe758a821
Pivi14 <pivarcsiadam@gmail.com>
2019-10-14 19:22:00 +0200

    Change board function, add main modul and function

--- function.py
+++ function.py
@@ -5,14 +5,37 @@ def init_board():
         matrix.append(row)
         for y in range(3):
             row.append(0)
-    return matrix
+    board = matrix
+    for i in range(len(matrix)):
+        for y in range(len(matrix[i])):
+            if matrix[i][y] == 0:
+                board[i][y] = '.'
+            elif matrix[i][y] == 1:
+                board[i][y] = 'X'
+            elif matrix[i][y] == 2:
+                board[i][y] = 'O'
+    return board
 
 def get_move():
-    pass
+    row = None
+    col = None
+    while row == 'A' or 'B' or 'C':
+        try:
+            row = input("Give me the row's letter: ").upper()
+        except:
+            continue
+    while col == 1 or 2 or 3:
+        try:
+            col = int(input("Give me the column's number: "))
+        except:
+            continue
+    coord = (row, col)
+    return coord
 
-def print_board():
-    board = [[' ', '.', ' ', '|', ' ', '.', ' ', '|', ' ', '.', ' '], ['---+---+---']]
-    print(*board[0], sep = '')
-    for i in range(2):
-        print(*board[1], '\n', *board[0], sep = '')
-
+def print_board(board):
+    print('   1   2   3')
+    print("A", ' {:1} | {:1} | {:1}'.format(board[0][0], board[0][1], board[0][2]))
+    print('  ---+---+---')
+    print("B", ' {:1} | {:1} | {:1}'.format(board[1][0], board[1][1], board[1][2]))
+    print('  ---+---+---')
+    print("C", ' {:1} | {:1} | {:1}'.format(board[2][0], board[2][1], board[2][2]))
---
+++ main.py
@@ -0,0 +1,7 @@
+import function
+
+def main():
+    board = function.init_board()
+    function.print_board(board)
+
+main()
