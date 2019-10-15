0cd5c43bdd89ab73df48d7f2b4a4abf83e4fcbfe
pivarcsi <pivarcsiadam@gmail.com>
2019-10-15 11:28:57 +0200

    main

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import function
 import random
 
 def main_menu():
-    pass
+    print('MENU')
+    print('(1) Player vs Player')
+    a = input("Give me the menu's number: ")
+    if a == 1:
+        game = 1
+    return game
 
-y = random.randrange(1, 3)
 
 def main():
-    game = 0
+    y = 1
+    game = 5
     board = function.init_board()
     while game == 0:
         function.print_board(board)
+        board = function.mark()
 
+main_menu()
 main()
